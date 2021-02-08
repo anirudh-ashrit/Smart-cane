517a7361e586b406777bb213387790de9ff9b68e
anirudh-ashrit <61005471+anirudh-ashrit@users.noreply.github.com>
2021-02-07 17:06:06 -0800

    The setup guide and blink test for ESP32

---
+++ ESP32_Setup/ESP32_Blink_Test/ESP32_Blink_Test.ino
@@ -0,0 +1,20 @@
+/*
+ * 
+ * Testing the ESP32 with Led Blink Code
+ * 
+ */
+
+int pin = 23;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin, HIGH);
+  delay(100);
+  digitalWrite(pin, LOW);
+  delay(100);
+}
